e3f05a7f83dc4498eb605f1cde40a32658262661
Karl Erik Bergaust <14124@norwegian.com>
2018-10-09 10:20:36 +0200

    first commit

--- backend/server.js
+++ backend/server.js
@@ -9,24 +9,24 @@ app.get('/', (request, response) => {
 });
 
 app.get('/api/me', (request, response) => {
-	response.send('{"name": "Arthur Dent", "location": "Earth"}');
+	response.send('{"name": "Aslack", "location": "Mars"}');
 	logger.info('Responded to GET request on /api/me')
 });
 
 app.get('/api/work', (request, response) => {
-	response.send('[{"yearFrom": "2010", "yearTo": "2018", "place": "BBC Radio", "comment": "Was doing stuff"}, {"yearFrom": "2009", "yearTo": "2010", "place": "The coffee shop", "comment": "Making coffee"}]');
+	response.send('[{"yearFrom": "2017", "yearTo": "2018", "place": "7N Mars", "comment": "Idle"}, {"yearFrom": "2009", "yearTo": "2010", "place": "The coffee shop", "comment": "Making coffee"}]');
 	logger.info('Responded to GET request on /api/work')
 });
 
 app.get('/api/education', (request, response) => {
-	response.send('[{"yearFrom": "2005", "yearTo": "2007", "place": "University 1", "comment": "Studying"}, {"yearFrom": "2001", "yearTo": "2004", "place": "Barista academy", "comment": "Made coffee"}]');
+	response.send('[{"yearFrom": "1972", "yearTo": "2001", "place": "Le Cole du Vie", "comment": "Studying"}, {"yearFrom": "2001", "yearTo": "2004", "place": "Barista academy", "comment": "Made coffee"}]');
 	logger.info('Responded to GET request on /api/edu')
 });
 
 app.listen(port, (err) => {
   if (err) {
-    return logger.error('something bad happened', err)
+    return logger.error('Ahh something bad happened', err)
   }
 
-  logger.info(`server is listening on ${port}`)
+  logger.info(`Ok Aslack is listening on ${port}`)
 });
